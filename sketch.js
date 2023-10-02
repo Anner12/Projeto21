192dfe629e884cc5a833e39fae2b8ba8cb4f8886
Anne Lie <103604990+Anner12@users.noreply.github.com>
2023-10-02 16:00:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball, groundObj, leftSide, rightSide;
+var world;
+var radius = 40;
+
+function setup() {
+	createCanvas(1600, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ball_options = {
+		isStatic: false, 
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
+	}
+
+	ball = Bodies.circle(260, 100, radius/2, ball_options);
+	World.add(world, ball);
+
+	groundObj = new ground(width/2, 670, width, 20);
+	leftSide = new ground(1100, 600, 20, 120);
+	rightSide = new ground(1350, 600, 20, 120);
+
+	Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  ellipse(ball.position.x, ball.position.y, radius, radius);
+
+  groundObj.display();
+  leftSide.display();
+  rightSide.display();
+}
+
+function keyPressed() {
+	if(keyCode=== UP_ARROW) {
+		Matter.Body.applyForce(ball, ball.position, {x: 85, y: -85	});
+	}
+}
+
